4003c07b906a7c462300c24066151aa4fecbdac3
George Matos <georgemattos@gmail.com>
2020-03-31 22:22:48 -0300

    tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storag.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(public storage: StorageService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
@@ -15,8 +18,19 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (!errorFromApi.status) { // se o error vier como texto puro, converte pra JSON (se tiver o attr status, então é um JSON)
           errorFromApi = JSON.parse(errorFromApi);
         }
+
+        switch (errorFromApi.status) {
+          case 403:
+          this.handle403();
+          break;
+        }
+
         return Observable.throw(errorFromApi);
       }) as any;
+  }
+
+  handle403() {
+    this.storage.setLocalUser(null);
   }
 
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,7 +29,13 @@ export class ProfilePage {
       .subscribe(response => {
         this.cliente = response;
         this.getImageIfExists();
-      }, error => {});
+      }, error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 

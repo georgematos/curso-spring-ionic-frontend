3611bdcdd194be072aa104360ccf420dbf9defec
George Matos <georgemattos@gmail.com>
2020-04-04 20:17:53 -0300

    criando pagina de sigup, FormGroup para controle do formulario e povando estado e cidade dinamicamente

---
+++ src/services/domain/estado.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { EstadoDTO } from "../../models/estado.dto";
+import { Observable } from "rxjs/Rx";
+
+@Injectable()
+export class EstadoService {
+
+  constructor(public http: HttpClient) {
+  }
+
+  findAll(): Observable<EstadoDTO[]> {
+    return this.http.get<EstadoDTO[]>(`${API_CONFIG.baseUrl}/estados`);
+  }
+
+  // findById(estado_id: string): Observable<EstadoDTO> {
+  //   return this.http.get<EstadoDTO>(`${API_CONFIG.baseUrl}/estados/${estado_id}`);
+  // }
+
+}

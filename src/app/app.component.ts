e11df60a9f93523388121276b2dd50619fb5afa9
George Matos <georgemattos@gmail.com>
2020-04-09 21:20:39 -0300

    acesso ao carrinho com botao flutunte e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ export class MyApp {
 
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      {title: 'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: '' }
     ];
 

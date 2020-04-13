9863e0cb72593f8b4128677c8132edf1d3e4da44
George Matos <georgemattos@gmail.com>
2020-04-13 10:02:38 -0300

    armazenamento dos dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
-import { ClienteDTO } from '../../models/cliente.dto';
 
 @IonicPage()
 @Component({
@@ -14,30 +16,46 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
   cliente: ClienteDTO;
+  pedido: PedidoDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public clienteService: ClienteService,
-    public storageService: StorageService
+    public storageService: StorageService,
+    public cartService: CartService
   ) { }
 
   ionViewDidLoad() {
     let localUser = this.storageService.getLocalUser();
+
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe((response) => {
           this.items = response['enderecos']; // poderia ser response.enderecos, mas o compilador reclamaria pos o objeto retornado nao está tipado
-        }),
+          let cart = this.cartService.getCart();
+          this.pedido = {
+            cliente: { id: response["id"] },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.itens.map(item => { // transforma cada item do carrinho em um objeto da lista de pedidos
+               return { quantidade: item.quantidade, produto: { id: item.produto.id } }
+            })
+          }
+        },
         (error: { status: number; }) => {
           if (error.status == 403) {
             this.navCtrl.setRoot('HomePage');
           }
-        };
+        });
     } else {
       this.navCtrl.setRoot('HomePage');
     }
+  }
 
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
   }
 
 }
